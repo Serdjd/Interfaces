3194cca1e674211dcc3372e05b628dc1534d5687
Serdjd <segiodobladomunoz@gmail.com>
2023-05-30 09:42:19 +0200

    se añaden modificaciones

--- src/App.java
+++ src/App.java
@@ -1,21 +1,23 @@
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.Border;
+import java.awt.event.*;
 public class App extends JFrame{
     private JButton boton;
     public App(){
         super("Rosa de los vientos");
-        setLayout(new BorderLayout());
-        add(new JButton("East"),BorderLayout.EAST);
-        add(new JButton("West"),BorderLayout.WEST);
-        add(new JButton("South"),BorderLayout.SOUTH);
-        add(new JButton("North"),BorderLayout.NORTH);
-        add(new JButton("Origen"),BorderLayout.CENTER);
+        boton = new JButton("Todavía no has pulsado");
+        add(boton);
+        boton.addActionListener(new OyenteBoton());
         setSize(200,200);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     public static void main(String[] args) throws Exception {
         App pestaña = new App();
+    }
+    class OyenteBoton implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            boton.setText("Pulsado");
+        }
     }
 }
